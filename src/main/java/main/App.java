7ccb5d8c043e5cf9765c71d33061019050ae85d5
Paulo Monterroso <84726204+pmont4@users.noreply.github.com>
2024-04-15 01:55:25 -0600

    Registro de NIT.

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,9 +1,14 @@
 package main;
 
+import frames.MainFrame;
+
 public class App {
     
+    private static MainFrame mainFrame;
+    
     public static void main(String[] args) {
-        
+        mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
     }
     
 }
